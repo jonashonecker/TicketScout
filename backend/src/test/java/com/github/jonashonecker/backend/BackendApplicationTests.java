e38e475e33c867508a104b0040a1e65538e13dbe
Jonas Honecker <honeckerjonas@gmail.com>
2024-06-13 11:46:02 +0200

    Add backend (Spring Boot) and frontend (Vite, React TypeScript)

---
+++ backend/src/test/java/com/github/jonashonecker/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jonashonecker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
