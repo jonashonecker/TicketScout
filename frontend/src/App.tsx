c055b0b72659e00647f4a19a895451b6151980b3
Jonas Honecker <honeckerjonas@gmail.com>
2024-06-19 09:05:31 +0200

    Add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import LoginPage from "./components/LoginPage.tsx";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./components/MainPage.tsx";
 
 export default function App() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <LoginPage />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </React.Fragment>
   );
 }
