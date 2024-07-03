a921b44f061a0eb2030b441ede7ea486a6dab02c
Jonas Honecker <honeckerjonas@gmail.com>
2024-07-03 14:48:40 +0200

    Adjusted frontend so error message is displayed on search as well.

--- frontend/src/components/forms/SearchForm.tsx
+++ frontend/src/components/forms/SearchForm.tsx
@@ -3,19 +3,28 @@ import SearchIcon from "@mui/icons-material/Search";
 import { Ticket } from "../../types/Ticket.ts";
 import { Dispatch, SetStateAction, FormEvent, useState } from "react";
 import { getAllTickets } from "../utils/ApiRequests.tsx";
+import { SnackbarConfig } from "../../types/Config.ts";
 
 type SearchFormProps = {
   setSearchResults: Dispatch<SetStateAction<Ticket[] | undefined>>;
+  setSnackbarConfig: Dispatch<SetStateAction<SnackbarConfig>>;
 };
 
 export default function SearchForm({
   setSearchResults,
+  setSnackbarConfig,
 }: Readonly<SearchFormProps>) {
   function searchTickets(event: FormEvent) {
     event.preventDefault();
-    getAllTickets(searchText).then((response) =>
-      setSearchResults(response.data),
-    );
+    getAllTickets(searchText)
+      .then((response) => setSearchResults(response.data))
+      .catch((error) => {
+        setSnackbarConfig({
+          open: true,
+          severity: "error",
+          message: error.response.data.error,
+        });
+      });
   }
 
   const [searchText, setSearchTest] = useState<string>("");
